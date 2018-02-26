5a33fb50e45581853895af381edb35cb1bc520d3
explosivenuke <wjstockr@mtu.edu>
2018-02-26 16:38:20 -0500

    added cat sprite as main player with 4 directional movement

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.awt.event.ActionEvent;
@@ -16,6 +17,17 @@ public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Texture img1;
+	Texture img2;
+	Texture img3;
+	Texture img4;
+	Texture img5;
+	boolean up;
+	boolean down;
+	boolean left;
+	boolean right;
+	int last = 0;
+	
+	
 	Player mainP = new Player(50,50,100);
 	int direction = 0;
 	ActionListener taskPerformer = new ActionListener() {
@@ -32,24 +44,37 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("greensquare.png");
 		img1 = new Texture("yellowsquare.jpg");
+		img2 = new Texture("0.png");
+		img3 = new Texture("1.png");
+		img4 = new Texture("2.png");
+		img5 = new Texture("3.png");
+		
 	}
 
 	@Override
 	public void render () {
 		if(Gdx.input.isKeyPressed(Keys.UP))
 		{
+			up = true;
+			last = 0;
 			mainP.setYCoordinate(mainP.getyCoordinate() + mainP.speed);
 		}
 		if(Gdx.input.isKeyPressed(Keys.DOWN))
 		{
+			last = 2;
+			down = true;
 			mainP.setYCoordinate(mainP.getyCoordinate() - mainP.speed);
 		}
 		if(Gdx.input.isKeyPressed(Keys.RIGHT))
 		{
+			last = 1;
+			right = true;
 			mainP.setXCoordinate(mainP.getxCoordinate() + mainP.speed);
 		}
 		if(Gdx.input.isKeyPressed(Keys.LEFT))
 		{
+			last = 3;
+			left = true;
 			mainP.setXCoordinate(mainP.getxCoordinate() - mainP.speed);
 		}
 //		if(direction == 1) mainP.setYCoordinate(mainP.getyCoordinate() + 1);
@@ -60,14 +85,60 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, mainP.getxCoordinate(), mainP.getyCoordinate());
+		if(up == true)
+		{
+		batch.draw(img2, mainP.getxCoordinate(), mainP.getyCoordinate());
+		}
+		else if(right == true)
+		{
+		batch.draw(img3, mainP.getxCoordinate(), mainP.getyCoordinate());
+		}
+		else if(down == true)
+		{
+		batch.draw(img4, mainP.getxCoordinate(), mainP.getyCoordinate());
+		}
+		else if(left == true)
+		{
+		batch.draw(img5, mainP.getxCoordinate(), mainP.getyCoordinate());
+		}
+		else
+		{
+			if(last == 0)
+			{
+			batch.draw(img2, mainP.getxCoordinate(), mainP.getyCoordinate());
+			}
+			if(last == 1)
+			{
+				batch.draw(img3, mainP.getxCoordinate(), mainP.getyCoordinate());
+			}
+			if(last == 2)
+			{
+				batch.draw(img4, mainP.getxCoordinate(), mainP.getyCoordinate());
+			}
+			if(last == 3)
+			{
+				batch.draw(img5, mainP.getxCoordinate(), mainP.getyCoordinate());
+			}
+		}
+		
+		
 		batch.draw(img1, 100, 100);
 		batch.end();
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		img1.dispose();
+		img2.dispose();
+		img3.dispose();
+		img4.dispose();
+		img5.dispose();
 	}
 }
